e71eeb3aecb14a1b0433f2f49824d6c50a6951bf
Seetraum <Seetraum@example.com>
2020-05-12 15:17:38 +0800

    测试线程池

---
+++ src/test/executorservice/Task.java
@@ -0,0 +1,12 @@
+package test.executorservice;
+
+public class Task  implements Runnable{
+    private int counter;
+    public Task(int counter){
+        this.counter = counter;
+    }
+    @Override
+    public void run() {
+        System.out.println("加入线程队列运行任务...." + counter);
+    }
+}
